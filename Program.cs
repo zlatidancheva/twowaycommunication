8e74453ad54d140891fd25dab6af6873d59db3f9
zlatidancheva <85891027+zlatidancheva@users.noreply.github.com>
2021-06-14 19:20:23 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,58 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 
-namespace client
+namespace Server
 {
-    public class ClientSocket
+    public class ServerSocket
     {
-        private Socket clientSocket;
         private int port;
+        private Socket serverSocket;
 
-        public ClientSocket(int port)
+        public ServerSocket(int port)
         {
             this.port = port;
-            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            /* Associates a Socket with a local endpoint. */
+            serverSocket.Bind(serverEndPoint);
+
+            /*Places a Socket in a listening state.
+             * The maximum length of the pending connections queue is 100 
+             */
+            serverSocket.Listen(100);
         }
 
         public void start()
         {
-            Console.WriteLine("Starting client socket");
-            try
+            Console.WriteLine("Starting the Server");
+
+            /* Accept Connection Requests */
+            Socket accepted = serverSocket.Accept();
+
+            /* Get the size of the send buffer of the Socket. */
+            int bufferSize = accepted.SendBufferSize;
+            byte[] buffer = new byte[bufferSize];
+
+            /* Receives data from a bound Socket into a receive buffer. It return the number of bytes received. */
+            int bytesRead = accepted.Receive(buffer);
+
+            byte[] formatted = new byte[bytesRead];
+
+            for (int i = 0; i < bytesRead; i++)
             {
-                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
-                clientSocket.Connect(serverEndPoint);
-
-                Console.WriteLine("Enter some data to send to server");
-
-                String data = Console.ReadLine();
-
-                byte[] bytes = Encoding.Unicode.GetBytes(data);
-
-                clientSocket.Send(bytes);
-
-                Console.WriteLine("Closing connection");
-
-                clientSocket.Close();
+                formatted[i] = buffer[i];
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error while connectiong to server {}", e.Message);
-            }
+
+            String receivedData = Encoding.Unicode.GetString(formatted);
+            Console.WriteLine("Received Data " + receivedData);
+
+            Console.WriteLine("Press some key to close");
+            Console.Read();
         }
     }
-
-    class Client
+    class Server
     {
         static void Main(string[] args)
         {
-            ClientSocket clientSocket = new ClientSocket(1234);
-            clientSocket.start();
-
+            ServerSocket server = new ServerSocket(1234);
+            server.start();
         }
     }
 }
+
